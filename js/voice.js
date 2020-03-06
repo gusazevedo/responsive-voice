82fcb56b92a40cfc95f8b81cbaedc68c02d547aa
Gustavo Azevedo <gustavonovaisgan@gmail.com>
2020-03-06 10:30:18 -0300

    aplicando leitura nos paragrafos

--- js/voice.js
+++ js/voice.js
@@ -1,5 +1,15 @@
 responsiveVoice.setDefaultVoice("Brazilian Portuguese Female");
 
 $('.falar').on('click', function(){
-	responsiveVoice.speak(document.getElementById('paragrafo').textContent);
+	// responsiveVoice.speak(document.getElementById('paragrafo').textContent);
+	const paragrafos = document.getElementsByClassName('paragrafo');
+	let texto = "";
+	for (let i = 0; i < paragrafos.length; i++){
+		texto = texto.concat(paragrafos[i].textContent)
+	}
+	responsiveVoice.speak(texto);
+});
+
+$('.parar').on('click', function(){
+	responsiveVoice.cancel();
 });
